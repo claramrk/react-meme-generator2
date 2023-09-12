3ad55821c31ea60c9426a3342c2c57b066286750
Clara Mrkos <mrkos.clara@gmail.com>
2023-09-12 16:00:05 +0200

    Set up project and skelleton

---
+++ src/App.js
@@ -0,0 +1,22 @@
+export default function App() {
+  return (
+    <>
+      <h1>Meme Generator</h1>
+      <div className="memePreview">
+        <p>XYZ</p>
+      </div>
+
+      <div className="textInput">
+        <p>XYZ</p>
+      </div>
+
+      <div className="memeTemplateInput">
+        <p>XYZ</p>
+      </div>
+
+      <div className="download">
+        <p>XYZ</p>
+      </div>
+    </>
+  );
+}
