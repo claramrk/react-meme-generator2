2363c4d05420d769a3d45693092080da90cfb61a
Clara Mrkos <mrkos.clara@gmail.com>
2023-09-12 16:36:12 +0200

    Build skelleton

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
+import React, { useEffect, useState } from 'react';
+
+let templateArray = [];
+
 export default function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.memegen.link/templates/`)
+      .then((response) => response.json())
+      .then((usefulData) => {
+        console.log(usefulData);
+        setLoading(false);
+        setData(usefulData);
+        templateArray = usefulData;
+      })
+      .catch((e) => {
+        console.error(`An error occurred: ${e}`);
+      });
+  }, []);
   return (
     <>
       <h1>Meme Generator</h1>
+      <div className="App">
+        {loading && <p>Loading...</p>}
+        {!loading && <p>Fetched data</p>}
+      </div>
+      <br />
+      <br />
       <div className="memePreview">
-        <p>XYZ</p>
+        <img href="" alt="background for meme" data-test-id="meme-image" />
       </div>
-
+      <br />
+      <br />
       <div className="textInput">
-        <p>XYZ</p>
+        <label htmlFor="Top Text">Top Text:</label>
+        <input name="Top Text" id="Top Text" />
+        <br />
+        <label htmlFor="Bottom Text">Bottom Text:</label>
+        <input name="Bottom Text" id="Bottom Text" />
       </div>
-
+      <br />
+      <br />
       <div className="memeTemplateInput">
-        <p>XYZ</p>
+        <label htmlFor="Templates">Meme Template</label>
+        <br />
+        <select value="Templates">
+          <option value="none"> - </option>
+          <option value="Template 1">Template 1</option>
+          <option value="Template 2">Template 2</option>
+        </select>
       </div>
-
+      <br />
+      <br />
       <div className="download">
-        <p>XYZ</p>
+        <button>Download</button>
       </div>
     </>
   );
