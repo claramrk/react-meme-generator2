48f9a3634223debe5ee61a8cbc5d6cacf60c0996
Clara Mrkos <mrkos.clara@gmail.com>
2023-09-13 14:06:51 +0200

    Update link to make it fillable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { saveAs } from 'file-saver';
 import React, { useEffect, useState } from 'react';
 
 let currentImage;
+let textOne;
+let textTwo;
 let generatedMemeURL;
+let currentImageSliced;
 
 export default function App() {
   const [image, setImage] = useState(null);
@@ -13,8 +16,13 @@ export default function App() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        currentImage = data[0].example.url;
-        setImage(currentImage);
+        currentImage = data[0].blank;
+        currentImageSliced = currentImage.slice(0, -4);
+        textOne = data[0].example.text[0];
+        textTwo = data[0].example.text[1];
+        generatedMemeURL = `${currentImageSliced}/${textOne}_/${textTwo}.png`;
+        console.log(generatedMemeURL);
+        setImage(generatedMemeURL);
       })
       .catch((e) => {
         console.error(`An error occurred: ${e}`);
@@ -26,46 +34,41 @@ export default function App() {
   };
 
   return (
-    <>
-      <header>
-        <h1>Meme Generator</h1>
-      </header>
-      <body>
-        <div className="memePreview">
-          <div className="image-frame">
-            <img
-              src={image}
-              alt="background for meme"
-              data-test-id="meme-image"
-            />
-          </div>
+    <body>
+      <div className="memePreview">
+        <div className="image-frame">
+          <img
+            src={image}
+            alt="background for meme"
+            data-test-id="meme-image"
+          />
         </div>
+      </div>
+      <br />
+      <br />
+      <div className="textInput">
+        <label htmlFor="Top Text">Top Text:</label>
+        <input name="Top Text" id="Top Text" />
         <br />
+        <label htmlFor="Bottom Text">Bottom Text:</label>
+        <input name="Bottom Text" id="Bottom Text" />
+      </div>
+      <br />
+      <br />
+      <div className="memeTemplateInput">
+        <label htmlFor="Templates">Meme Template</label>
         <br />
-        <div className="textInput">
-          <label htmlFor="Top Text">Top Text:</label>
-          <input name="Top Text" id="Top Text" />
-          <br />
-          <label htmlFor="Bottom Text">Bottom Text:</label>
-          <input name="Bottom Text" id="Bottom Text" />
-        </div>
-        <br />
-        <br />
-        <div className="memeTemplateInput">
-          <label htmlFor="Templates">Meme Template</label>
-          <br />
-          <select value="Templates">
-            <option value="none"> - </option>
-            <option value="Template 1">Template 1</option>
-            <option value="Template 2">Template 2</option>
-          </select>
-        </div>
-        <br />
-        <br />
-        <div className="download">
-          <button onClick={handleClick}>Download</button>
-        </div>
-      </body>
-    </>
+        <select value="Templates">
+          <option value="none"> - </option>
+          <option value="Template 1">Template 1</option>
+          <option value="Template 2">Template 2</option>
+        </select>
+      </div>
+      <br />
+      <br />
+      <div className="download">
+        <button onClick={handleClick}>Download</button>
+      </div>
+    </body>
   );
 }
