78db27579cd910e3d4aa7a07aeed1c0599211cc2
Clara Mrkos <mrkos.clara@gmail.com>
2023-09-15 11:32:28 +0200

    Add favicon

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    <head>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/icon-apple-touch.png" />
+      <link rel="manifest" href="/manifest.json" />
+    </head>
     <App />
   </React.StrictMode>,
 );
